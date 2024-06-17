955334639b1caf0c51b57037bc3b6c5a4204226c
Sean Harrison <sharrison9@gmail.com>
2024-06-17 17:44:42 -0500

    landing page styling and formatting

--- src/components/LandingRegistration/LandingRegistration.jsx
+++ src/components/LandingRegistration/LandingRegistration.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import RegisterForm from '../RegisterForm/RegisterForm';
 import './LandingRegistration.css';
+import RegisterForm from '../RegisterForm/RegisterForm';
 
 function LandingRegistration() {
   const [heading, setHeading] = useState('Welcome');
@@ -14,17 +14,12 @@ function LandingRegistration() {
   return (
     <div className="container">
       <h2>{heading}</h2>
-
       <div className="grid">
         <div className="grid-col grid-col_8">
           <p>
             Welcome to the Movie Club App! This platform is designed to bring friends together in small movie clubs to discuss films on a bimonthly basis. Whether you're a casual moviegoer or a dedicated cinephile, our app provides the perfect environment for sharing your thoughts and opinions on the latest releases and classic films alike.
           </p>
-
-          <p>
-            Here are some of the key features of the Movie Club App:
-          </p>
-
+          <p>Here are some of the key features of the Movie Club App:</p>
           <ul>
             <li>
               <strong>Create or Join Clubs:</strong> Easily create a new movie club or join an existing one with your friends. Each club can have up to four members, ensuring intimate and engaging discussions.
@@ -45,14 +40,10 @@ function LandingRegistration() {
               <strong>Integrations:</strong> Seamlessly integrate with popular movie databases to fetch information about movies, including synopses, cast details, and trailers.
             </li>
           </ul>
-
-          <p>
-            Join the Movie Club App today and start enjoying movies with friends like never before!
-          </p>
+          <p>Join the Movie Club App today and start enjoying movies with friends like never before!</p>
         </div>
         <div className="grid-col grid-col_4">
           <RegisterForm />
-
           <center>
             <h4>Already a Member?</h4>
             <button className="btn btn_sizeSm" onClick={onLogin}>
