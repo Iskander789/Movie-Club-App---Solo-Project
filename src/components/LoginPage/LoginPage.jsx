2ab2fbaa1c6be0a0dc13a7835216f1c5699a7f54
Sean Harrison <sharrison9@gmail.com>
2024-06-17 23:21:04 -0500

    Updated LoginPage.jsx to correctly navigate to LandingRegistration page when the Register button is clicked.

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,25 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import LoginForm from '../LoginForm/LoginForm';
-import { useSelector } from 'react-redux';
-import { Redirect, useHistory } from 'react-router-dom';
 
 function LoginPage() {
-  const user = useSelector((store) => store.user);
   const history = useHistory();
-
-  if (user.id) {
-    return <Redirect to="/user" />;
-  }
 
   return (
     <div>
       <LoginForm />
+
       <center>
         <button
           type="button"
           className="btn btn_asLink"
           onClick={() => {
-            history.push('/registration');
+            history.push('/landing');
           }}
         >
           Register
