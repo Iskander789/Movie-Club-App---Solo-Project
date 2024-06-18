5220c9a33063d368f559dd674cd0cb75fc0c95e8
Sean Harrison <sharrison9@gmail.com>
2024-06-17 23:39:20 -0500

    fix functionality in Nav Bar logout

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 import './Nav.css';
 
 function Nav() {
   const location = useLocation();
   const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
   const isLoggedIn = Boolean(user.id);
+
+  const handleLogout = () => {
+    dispatch({ type: 'LOGOUT' });
+  };
 
   return (
     <nav className="nav">
       <div className="nav-title">Movie Club</div>
       <ul style={{ display: 'flex', listStyle: 'none', margin: 0, padding: 0 }}>
+        {!isLoggedIn && location.pathname !== '/' && (
+          <li>
+            <Link to="/" className="navLink">Landing</Link>
+          </li>
+        )}
         {isLoggedIn && location.pathname !== '/home' && (
           <li>
             <Link to="/home" className="navLink">Home</Link>
@@ -35,19 +45,14 @@ function Nav() {
         )}
         {isLoggedIn ? (
           <li>
-            <Link to="/logout" className="navLink">Log Out</Link>
+            <button onClick={handleLogout} className="navLink">Log Out</button>
           </li>
         ) : (
-          location.pathname !== '/login' && location.pathname !== '/landing' && (
+          location.pathname !== '/login' && (
             <li>
               <Link to="/login" className="navLink">Login</Link>
             </li>
           )
-        )}
-        {!isLoggedIn && location.pathname !== '/registration' && location.pathname !== '/login' && (
-          <li>
-            <Link to="/registration" className="navLink">Register</Link>
-          </li>
         )}
       </ul>
     </nav>
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 
 function UserPage() {
-  const user = useSelector((store) => store.user);
-  const history = useHistory();
-
-  const handleLogout = () => {
-    // Logic to log out the user
-  };
+  const user = useSelector((state) => state.user);
 
   return (
-    <div className="container">
+    <div>
       <h1>Welcome, {user.username}!</h1>
-      {/* Remove the "Go to Profile" link from here */}
-      <button onClick={handleLogout}>Log Out</button>
+      {/* Other components and content */}
     </div>
   );
 }
