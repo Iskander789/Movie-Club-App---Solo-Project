61ca4f0c206f0894b2a98c518e62f0affc6bb513
Sean Harrison <sharrison9@gmail.com>
2024-06-22 21:40:30 -0500

    conditional rendering for nav bar based on log in status

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -8,53 +8,43 @@ function Nav() {
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
-  const isLoggedIn = Boolean(user.id);
-
   const handleLogout = () => {
     dispatch({ type: 'LOGOUT' });
   };
 
   return (
     <nav className="nav">
-      <div className="nav-title">Movie Club</div>
-      <div className="nav-links-container">
-        <ul>
-          {isLoggedIn && (
-            <>
-              <li>
-                <Link to="/home" className={`navLink ${location.pathname === '/home' ? 'active' : ''}`}>
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/groups" className={`navLink ${location.pathname === '/groups' ? 'active' : ''}`}>
-                  Your Groups
-                </Link>
-              </li>
-              <li>
-                <Link to="/profile" className={`navLink ${location.pathname === '/profile' ? 'active' : ''}`}>
-                  Profile
-                </Link>
-              </li>
-              <li>
-                <Link to="/about" className={`navLink ${location.pathname === '/about' ? 'active' : ''}`}>
-                  About This App
-                </Link>
-              </li>
-              <li>
-                <Link to="/technologies" className={`navLink ${location.pathname === '/technologies' ? 'active' : ''}`}>
-                  Technologies Used
-                </Link>
-              </li>
-              <li>
-                <Link to="/login" className="navLink" onClick={handleLogout}>
-                  Log Out
-                </Link>
-              </li>
-            </>
-          )}
-        </ul>
+      <div className="nav-title">
+        <Link to={user.id ? "/home" : "/landing"} className="navLink-title">
+          Movie Club
+        </Link>
       </div>
+      {user.id && (
+        <div className="nav-links-container">
+          <ul>
+            <li>
+              <Link to="/home" className={`navLink ${location.pathname === '/home' ? 'active' : ''}`}>Home</Link>
+            </li>
+            <li>
+              <Link to="/groups" className={`navLink ${location.pathname === '/groups' ? 'active' : ''}`}>Your Groups</Link>
+            </li>
+            <li>
+              <Link to="/profile" className={`navLink ${location.pathname === '/profile' ? 'active' : ''}`}>Profile</Link>
+            </li>
+            <li>
+              <Link to="/about" className={`navLink ${location.pathname === '/about' ? 'active' : ''}`}>About This App</Link>
+            </li>
+            <li>
+              <Link to="/technologies-used" className={`navLink ${location.pathname === '/technologies-used' ? 'active' : ''}`}>Technologies Used</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to="/login" className="navLink" onClick={handleLogout}>Log Out</Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 }
